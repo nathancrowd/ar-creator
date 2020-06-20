7ec10568c1f9ebb2d6df1b466685d3cd642a03f5
Nathan <nathan@thisiscrowd.com>
2020-06-20 17:01:55 +0100

    Undo some git changes

---
+++ experiences/duck/index.js
@@ -0,0 +1,6 @@
+
+            const assets = [{"model":"ducky.gltf","patt":"markerv6.patt"},{"model":"ducky.gltf","patt":"pattern-lorem-marker.patt"}];
+            module.exports = {
+                assets
+            }
+        
