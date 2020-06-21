9dcd5f6943904369e4fda2cb45bc1bab573ac551
Nathan <nathan@thisiscrowd.com>
2020-06-21 19:10:24 +0100

    Fix buggy form POST

--- experiences/duck/index.js
+++
@@ -1,6 +0,0 @@
-
-            const assets = [{"model":"ducky.gltf","patt":"markerv6.patt"},{"model":"ducky.gltf","patt":"pattern-lorem-marker.patt"}];
-            module.exports = {
-                assets
-            }
-        
